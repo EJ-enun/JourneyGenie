cc9a834528be3a549b551326e3c816f8979bc027
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-06-03 10:52:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ def set_logo():
     htp = "https://raw.githubusercontent.com/EJ-enun/journeygenie/main/logo.png"
     col1, col2, col3 = st.columns([1,6,1])
     with col2:
-        return st.image(htp, caption='Dress for the Weather, Impress with Style.')
+        return st.image(htp, caption='Navigate the visa application process and chart your travel journey with ease.
+')
         
 def main():
     
