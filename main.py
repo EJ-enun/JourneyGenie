144ddd6c30597f76f1f08ed5d507cdc682f3da08
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-06-05 13:14:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 from google.cloud import aiplatform 
 import vertexai
-from vertexai.text_generation import TextGenerationModel
+from vertexai.generativemodels import GenerativeModel
 import pyperclip
 # Replace with your project ID and location
 PROJECT_ID = "visavoyage"
